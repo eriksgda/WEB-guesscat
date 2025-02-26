777fb4d8138cbca7309e0816174378f0bdf598a7
eriksgda <erik.ksgda@gmail.com>
2025-02-26 18:35:34 -0300

    feat: add methods to buttons and service to connect to the api

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { AuthLayoutComponent } from '../../components/auth-layout/auth-layout.component';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
+import { Route, Router } from '@angular/router';
+import { LoginService } from '../../services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -10,16 +12,33 @@ import { PrimaryInputComponent } from '../../components/primary-input/primary-in
     ReactiveFormsModule,
     PrimaryInputComponent
   ],
+  providers: [
+    LoginService
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
   loginForm: FormGroup;
 
-  constructor() {
+  constructor(
+    private router: Router,
+    private loginService: LoginService
+  ) {
     this.loginForm = new FormGroup({
       username: new FormControl('', [Validators.required, Validators.minLength(6)]),
       password: new FormControl('', [Validators.required, Validators.minLength(6)])
     })
+  }
+
+  submit() {
+    this.loginService.login(this.loginForm.value.username, this.loginForm.value.password).subscribe({
+      next: () => console.log("success"),
+      error: () => console.log("error")
+    });
+  }
+
+  navegate() {
+    this.router.navigate(["singup"]);
   }
 }
