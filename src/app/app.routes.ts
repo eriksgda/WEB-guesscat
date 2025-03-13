bf44878f5016c383bd97fe7d8cfb3c85fd6daac3
eriksgda <erik.ksgda@gmail.com>
2025-03-12 22:00:06 -0300

    feat: add reverse auth guard && change guard to user unnauthenticated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,20 +2,23 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { GameComponent } from './pages/game/game.component';
-import { AuthGuardService } from './services/auth-guard.service';
+import { authguardGuard } from './services/guard/authguard.guard';
+import { reverseAuthguardGuard } from './services/guard/reverse-authguard.guard';
 
 export const routes: Routes = [
     {
         path:"login",
-        component: LoginComponent
+        component: LoginComponent,
+        canActivate: [reverseAuthguardGuard]
     },
     {
         path:"signup",
-        component: SignupComponent
+        component: SignupComponent,
+        canActivate: [reverseAuthguardGuard]
     },
     {
         path:"game",
         component: GameComponent,
-        canActivate: [AuthGuardService]
+        canActivate: [authguardGuard]
     }
 ];
