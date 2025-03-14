3bbc8373144035ba04080fb2f42c3bf87a080fcc
eriksgda <erik.ksgda@gmail.com>
2025-03-13 22:49:22 -0300

    feat: add click event to start a new game

--- src/app/components/user-asidebar/user-asidebar.component.ts
+++ src/app/components/user-asidebar/user-asidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-user-asidebar',
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './user-asidebar.component.scss'
 })
 export class UserAsidebarComponent {
+  @Output() newGameEvent = new EventEmitter<void>();
 
+  startGame(): void {
+    this.newGameEvent.emit();
+  }
 }
