3913c0d32dc8dcdf0194d0d587a24c8fc46dbe52
eriksgda <erik.ksgda@gmail.com>
2025-03-13 22:43:07 -0300

    feat: create game service

---
+++ src/app/services/game.service.ts
@@ -0,0 +1,45 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { GameResultDTO } from '../types/game-resultDTO.type';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GameService {
+
+  private apiUrl: string = "http://localhost:8080/game";
+
+  constructor(private httpClient: HttpClient, private authService: AuthService) { }
+
+  startGame(): Observable<{ word: string }>{
+    const token: string | null = this.authService.getToken();
+
+    if (!token) {
+      console.error("Nedd authentication to continue.");
+      return new Observable(observer => observer.error("No authentication token."));;
+    }
+
+    const headers = new HttpHeaders({
+      "Authorization": `Bearer ${token}`
+    });
+
+    return this.httpClient.get<{word: string}>(`${this.apiUrl}/start`, {headers: headers});
+  }
+
+  postResult(dto: GameResultDTO): Observable<any>{
+    const token: string | null = this.authService.getToken();
+
+    if (!token) {
+      console.error("Nedd authentication to continue.");
+      return new Observable(observer => observer.error("No authentication token."));;
+    }
+
+    const headers = new HttpHeaders({
+      "Authorization": `Bearer ${token}`
+    });
+
+    return this.httpClient.post(`${this.apiUrl}/done`, dto, {headers: headers});
+  }
+}
