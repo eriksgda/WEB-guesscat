898fded64523aee2d26b6b058812dd20f462addd
eriksgda <erik.ksgda@gmail.com>
2025-03-19 22:15:17 -0300

    feat: create user service to get user match history

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
+import { Observable } from 'rxjs';
+import { MatchHistoryDTO } from '../types/match-historyDTO.type';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private apiUrl: string = "http://localhost:8080/cat/account";
+
+  constructor(private httpClient: HttpClient, private authService: AuthService) { }
+
+  getUserMatchHistory(): Observable<MatchHistoryDTO>{
+    const token: string | null = this.authService.getToken();
+
+    if (!token) {
+      console.error("Nedd authentication to continue.");
+      return new Observable(observer => observer.error("No authentication token."));;
+    }
+
+    const headers = new HttpHeaders({
+      "Authorization": `Bearer ${token}`
+    });
+
+    return this.httpClient.get<MatchHistoryDTO>(`${this.apiUrl}/history`, {headers: headers});
+  }
+}
