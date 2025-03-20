861e2be16433b18b2f97c5b9c8e7370e9c344b49
eriksgda <erik.ksgda@gmail.com>
2025-03-19 22:13:43 -0300

    feat: add match history button event and username in the asidebar

--- src/app/components/user-asidebar/user-asidebar.component.ts
+++ src/app/components/user-asidebar/user-asidebar.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-user-asidebar',
@@ -6,10 +7,30 @@ import { Component, EventEmitter, Output } from '@angular/core';
   templateUrl: './user-asidebar.component.html',
   styleUrl: './user-asidebar.component.scss'
 })
-export class UserAsidebarComponent {
+export class UserAsidebarComponent implements OnInit{
   @Output() newGameEvent = new EventEmitter<void>();
+  @Output() newHistoryEvent = new EventEmitter<void>();
+
+  username: string = "GUESSCATGAME";
+  
+  constructor(private authService: AuthService) {
+  }
+
+  ngOnInit(): void {
+    const aux = this.authService.getUsername();
+
+    if (!aux) {
+      return;
+    }
+
+    this.username = aux;
+  }
 
   startGame(): void {
     this.newGameEvent.emit();
+  }
+
+  viewMatchHistory(): void {
+    this.newHistoryEvent.emit();
   }
 }
