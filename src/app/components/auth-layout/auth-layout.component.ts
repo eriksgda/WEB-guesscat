7bc80e328738409188efcd89127c3284c1958199
eriksgda <erik.ksgda@gmail.com>
2025-07-08 23:11:36 -0300

    feat: create reusable modal component with enum-based content

--- src/app/components/auth-layout/auth-layout.component.ts
+++ src/app/components/auth-layout/auth-layout.component.ts
@@ -1,8 +1,12 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ModalType } from '../../types/modal-type.enum';
+import {ModalComponent} from '../modal/modal.component';
 
 @Component({
   selector: 'app-auth-layout',
-  imports: [],
+  imports: [
+    ModalComponent
+  ],
   templateUrl: './auth-layout.component.html',
   styleUrl: './auth-layout.component.scss'
 })
@@ -22,4 +26,14 @@ export class AuthLayoutComponent {
   navigate() {
     this.onNavigate.emit();
   }
+
+  isModalOpen: boolean = false;
+  type: ModalType = ModalType.CLOSE;
+
+  toggleModal(type: ModalType) {
+    this.type = type;
+    this.isModalOpen = !this.isModalOpen;
+  }
+
+  protected readonly ModalType = ModalType;
 }
---
+++ src/app/types/modal-type.enum.ts
@@ -0,0 +1,6 @@
+export enum ModalType{
+  LEARN = "Learn",
+  LANGUAGE = "Language",
+  CONTACTS = "Contacts",
+  CLOSE = ""
+}
