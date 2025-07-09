e3f340d55483bea4c84076541f12ee32db09b4a1
eriksgda <erik.ksgda@gmail.com>
2025-07-09 10:55:30 -0300

    feat: add lucide icons imports

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,21 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideToastr } from 'ngx-toastr';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import {CircleQuestionMark, Ellipsis, Languages, LucideAngularModule, Menu, X} from 'lucide-angular';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(withFetch()),
     provideToastr(),
-    provideAnimations()
+    provideAnimations(),
+    importProvidersFrom(LucideAngularModule.pick(
+      { Languages, X, Menu, CircleQuestionMark, Ellipsis }
+    ))
   ]
 };
