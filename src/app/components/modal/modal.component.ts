05b956369c93172f7c52555cd522d84cad5798aa
eriksgda <erik.ksgda@gmail.com>
2025-07-17 21:58:09 -0300

    feat: add specific modals to win and defeat cases

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,6 +1,7 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {ModalType} from '../../types/enums/modal-type.enum';
 import {LucideAngularModule} from 'lucide-angular';
+import {GameResponseDTO} from '../../types/dto/game-responseDTO.type';
 
 @Component({
   selector: 'app-modal',
@@ -13,6 +14,8 @@ import {LucideAngularModule} from 'lucide-angular';
 export class ModalComponent {
   @Input() type!: ModalType;
   @Output() closed = new EventEmitter();
+
+  @Input() possibleContent?: GameResponseDTO;
 
   onClose() {
     this.closed.emit();
