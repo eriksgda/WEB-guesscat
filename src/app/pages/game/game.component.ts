303414012ddba1124ef0889ce4c21faf636a1f98
eriksgda <erik.ksgda@gmail.com>
2025-07-17 21:53:40 -0300

    refactor: change the game page to control only modals and contents using enums and @switch

--- src/app/pages/game/game.component.ts
+++ src/app/pages/game/game.component.ts
@@ -1,150 +1,41 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import { UserAsidebarComponent } from "../../components/user-asidebar/user-asidebar.component";
-import { CommonModule, NgFor,NgIf } from '@angular/common';
-import { LetterStatus } from '../../types/letter-status.model';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { GameService } from '../../services/game.service';
-import { GameResultDTO } from '../../types/game-resultDTO.type';
-import { MatchHistoryDTO } from '../../types/match-historyDTO.type';
-import { UserService } from '../../services/user.service';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import {ContentFlag} from '../../types/enums/content-flag.enum';
+import {GuessesComponent} from '../../components/guesses/guesses.component';
 
 @Component({
   selector: 'app-game',
   imports: [
-    UserAsidebarComponent, 
+    UserAsidebarComponent,
     ReactiveFormsModule,
     CommonModule,
-    NgFor, 
-    NgIf
+    GuessesComponent,
   ],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
 export class GameComponent{
-  word: string = '';
-  length: number = 0;
-  maxGuesses: number = 0;
-  guesses: string[] = [];
-  gameOver: boolean = false;
-  playedIn!: Date;
-  gameForm!: FormGroup;
-  grid: LetterStatus[][] = [];
-  gameStarted: boolean = false;
-  matchHistory: boolean = false;
-  matchHistoryData!: MatchHistoryDTO;
-
-  constructor(private gameService: GameService,private userService: UserService, private formBuilder: FormBuilder) {
-    this.gameForm = this.formBuilder.group({
-      guess: ["", [Validators.required, Validators.minLength(1)]]
-    });
+  constructor() {
   }
 
-  getMatchHistory(): void {
-    this.userService.getUserMatchHistory().subscribe(response => {
-      if (!response || !response.username && !response.matchHistory) {
-        console.error("Error.");
-        return;
-      } if (response.matchHistory.length === 0) {
-        this.matchHistory = false;
-        return;
-      } 
-      this.gameStarted = false;
-      this.matchHistoryData = response;
+  public contentFlag?: ContentFlag;
+  protected readonly ContentFlag = ContentFlag;
 
-      this.matchHistory = true;
-    })
+  public newGameButtonFLag: boolean = false;
+
+  startGame() {
+    this.newGameButtonFLag = true;
+
+    this.contentFlag = ContentFlag.GAME;
   }
 
-  startGame(): void {
-    this.gameService.startGame().subscribe(response => {
-      if (!response || !response.word) {
-        console.error("Error.");
-        return;
-      }
-
-      this.matchHistory = false;
-      this.gameOver = false;
-      
-      this.word = response.word.toLowerCase();
-      this.length = this.word.length;
-      this.maxGuesses = this.length;
-
-      this.playedIn = new Date();
-
-      this.grid.length = 0;
-      this.guesses.length = 0;
-
-      for (let i = 0; i < this.maxGuesses; i++) {
-        this.grid.push(Array(this.length).fill({letter: "", status: "default"}));
-      }
-
-      this.gameStarted = true;
-    })
+  onGameFinished() {
+    this.newGameButtonFLag = false;
   }
 
-
-  submitGuess(): void {
-    if (this.gameOver) {return; }
-
-    const guess = this.gameForm.value.guess.toLowerCase();
-
-    if (guess.length !== this.length) {
-      alert(`The guess must contain ${this.length} letters.`);
-      return;
-    }
-
-    this.guesses.push(guess);
-    const indexRow = this.guesses.length -1;
-    this.grid[indexRow] = this.validateGuess(guess);
-
-    if (guess === this.word || this.guesses.length === this.maxGuesses) {
-      this.gameOver = true;
-      this.postResult();
-    }
-
-    this.gameForm.reset();
-  }
-
-
-  validateGuess(guess: string): LetterStatus[] {
-    const result: LetterStatus[] = [];
-    const wordArray = this.word.split('');
-    const checked: boolean[] = new Array(this.length).fill(false);
-
-    for (let i = 0; i< this.length; i++) {
-      if (guess[i] === wordArray[i]) {
-        result.push({letter: guess[i], status: "correct"});
-        checked[i] = true;
-      } else {
-        result.push({letter: guess[i], status: "default"});
-      }
-    }
-
-    for (let i = 0; i < this.length; i++) {
-      if (result[i].status === "correct") {continue;}
-
-      const index = wordArray.findIndex((l, j) => l === guess[i] && !checked[j]);
-
-      if (index !== -1) {
-        result[i].status = "present";
-        checked[index] = true;
-      }
-    }
-
-    return result;
-  }
-
-  postResult(): void {
-    const dto: GameResultDTO = {
-      word: this.word,
-      guesses: this.guesses,
-      playedIn: this.playedIn.toISOString()
-    };
-
-    this.gameService.postResult(dto).subscribe(response => {
-      console.log("Result post successfully", response);
-    }, error => {
-      console.log("Error", error);
-    });
+  getMatchHistory() {
+    this.contentFlag = ContentFlag.HISTORY;
   }
 }
