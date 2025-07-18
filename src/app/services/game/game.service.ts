8cc2255cf71f12002d7418258083f522c1e2a679
eriksgda <erik.ksgda@gmail.com>
2025-07-17 21:56:29 -0300

    refactor: move game service to game package

--- src/app/services/game/game.service.ts
+++ src/app/services/game/game.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { GameResultDTO } from '../types/dto/game-resultDTO.type';
-import { AuthService } from './auth.service';
+import { GameResultDTO } from '../../types/dto/game-resultDTO.type';
+import { AuthService } from '../auth.service';
 
 @Injectable({
   providedIn: 'root'
